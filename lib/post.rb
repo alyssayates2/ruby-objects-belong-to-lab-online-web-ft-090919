9bffc3fa61ce926a484976f104899aba346edb9f
Alyssa Yates <bosslys@icloud.com>
2019-09-22 22:05:40 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,23 +1,23 @@
 class Post
-  attr_accessor :title, :author 
+  attr_accessor :title, :author
 
   def initialize(title, author)
-    @title = title 
-    @author = author 
+    @title = title
+    @author = author
   end 
-end 
+end
 
 class Artist
   attr_accessor :name, :genre
- 
-  def initialize(name, genre) 
+
+  def initialize(name, genre)
     @name = name
     @genre = genre
   end
- 
+
 end
- 
+
 drake = Artist.new("Drake", "rap")
 hotline_bling = Song.new("Hotline Bling")
- 
+
 hotline_bling.artist = drake
