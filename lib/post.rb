4d34a989a10d1c338a3136424362da57683d967d
Alyssa Yates <bosslys@icloud.com>
2019-09-22 22:09:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post
   attr_accessor :title, :author
 
-  def initialize(title, author)
+  def initialize
     @title = title
     @author = author
   end
@@ -9,4 +9,3 @@ class Post
 end
 
 
-drake = Post.new("Drake", "rap")
